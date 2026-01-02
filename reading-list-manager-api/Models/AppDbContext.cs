cb9bbc624e8355bf2dda4daac5fe86d4b6b9fb1e
samuel <samuelcarlos181@gmail.com>
2026-01-02 18:26:43 -0300

    fix: book-debset

--- reading-list-manager-api/Models/AppDbContext.cs
+++ reading-list-manager-api/Models/AppDbContext.cs
@@ -7,7 +7,7 @@
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Book> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
         public DbSet<ReadingList> ReadingLists { get; set; }
     }
 }
